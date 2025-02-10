16bbcaab4c57e0b00732c940cfb1068edbad8688
Robendl <robendelange@gmail.com>
2025-02-10 13:00:09 +0100

    full base timevae run

--- shell/run.sh
+++ shell/run.sh
@@ -6,12 +6,12 @@
 #SBATCH --ntasks=1
 #SBATCH --mem=32GB
 #SBATCH --job-name=udl
-#SBATCH --output=interpretable1.out
+#SBATCH --output=udl-interpretable-last-idx.out
 
 module purge
 module load Python/3.11.5-GCCcore-13.2.0
 
-source ./udl/bin/activate
+source udl/bin/activate
 
 python3 src/generate_scores.py
 
